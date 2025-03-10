b0c4e547a6abad9cb7b79d45ec95411e3f8d418e
Riddhi2909 <upadhyayriddhi445@gmail.com>
2025-03-10 18:24:32 +0530

    project completed

--- src/app/dashboard/_components/Header.jsx
+++ src/app/dashboard/_components/Header.jsx
@@ -7,13 +7,23 @@ function Header() {
   return (
     <div className ='p-4 shadow-sm border flex justify-end gap-2'>
       <div className='flex items-center gap-2'>
-      <Image
-        src={user?.picture}
-        width={35}
-        height={35}
-        alt="user"
-        className='rounded-full'
-      />
+      {user?.picture && user.picture !== "" ? (
+        <Image
+          src={user.picture}
+          width={35}
+          height={35}
+          alt="user"
+          className="rounded-full"
+        />
+      ) : (
+        <Image
+          src="/user.svg"
+          width={35}
+          height={35}
+          alt="default user"
+          className="rounded-full"
+        />
+      )}
       </div>
     </div>
   )
