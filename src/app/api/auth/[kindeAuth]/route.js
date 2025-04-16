adedabfabfbd5065bf395a080b6f178c301d6e52
Riddhi2909 <upadhyayriddhi445@gmail.com>
2025-04-16 13:14:51 +0530

    project is completed

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -2,4 +2,4 @@ import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const GET = handleAuth({
     kindeIssuerUrl: process.env.KINDE_ISSUER_URL, // Explicitly pass the variable
-});
+}); 
